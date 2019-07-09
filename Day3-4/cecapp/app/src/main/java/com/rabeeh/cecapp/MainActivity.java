f4d8fcaea804a47456a5a1d6ebbb6b4605577a0d
rabeehrz <mohammadrabeeh@gmail.com>
2019-07-09 13:39:31 +0530

    Add: Day 4

--- Day3-4/cecapp/app/src/main/java/com/rabeeh/cecapp/MainActivity.java
+++ Day3-4/cecapp/app/src/main/java/com/rabeeh/cecapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.rabeeh.cecapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     EditText etFirst, etSecond;
     Button btnAdd, btnMinus, btnMultiply, btnDivide, btnClear;
     TextView tvResult;
-    int a, b;
+    int a, b, c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +36,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 try {
-                    a = Integer.parseInt(etFirst.getText().toString());
-                    b = Integer.parseInt(etSecond.getText().toString());
-                    tvResult.setText(Integer.toString(a + b));
+                    new AddTask().execute();
+//                    a = Integer.parseInt(etFirst.getText().toString());
+//                    b = Integer.parseInt(etSecond.getText().toString());
+//                    c = add(a,b);
+//                    tvResult.setText(Integer.toString(c));
                 } catch (Exception e) {
                     showErrorToast("Enter a number");
                 }
@@ -97,5 +100,34 @@ public class MainActivity extends AppCompatActivity {
 
     public void showErrorToast(String text) {
         Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    class AddTask extends AsyncTask {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            tvResult.setText("Please Wait!");
+        }
+
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            String epoch = String.valueOf(System.currentTimeMillis());
+            return epoch;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+            tvResult.setText((String) o);
+        }
     }
 }
